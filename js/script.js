13e1b00794dbdac4e77fe28e0f988f10f31cbbb4
IlCarretto <carlorebuffat412@gmail.com>
2022-10-10 16:51:22 +0200

    finished exercise (no bonus)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-alert("ciao");
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Lorem” al posto del numero e per i multipli di 5 stampi “Ipsum”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “LoremIpsum”.
+
+for (let i = 1; i <= 100; i++) {
+    // memorizzo il numero
+    let iResult = "";
+    // SE numero è multiplo di 3 e 5, memorizzo fizzbuzz;
+    if (i % 3 === 0 && i % 5 === 0) {
+        iResult = "LoremIpsum";
+        // ALTRIMENTI SE numero è multiplo di 3, memorizzo fizz;
+    } else if (i % 3 === 0) {
+        iResult = "Lorem";
+        // ALTRIMENTI SE numero è multiplo di 5, memorizzo buzz;
+    } else if (i % 5 === 0 ) {
+        iResult = "Ipsum";
+    } else {
+        // ALTRIMENTI memorizzo numero;
+        iResult = i;
+    }
+    console.log(iResult);
+}
+
+for (let i = 0; i <= 100; i++) {
+    // Se numero è pari, memorizzo pari
+    // Altrimenti memorizzo dispari
+    let pariDispari = "";
+    if (i % 2 === 0) {
+      pariDispari = "pari";
+    } else {
+      pariDispari = "dispari";
+    }}
